e89ad77b72607698bc9ff7ed413da223e4e31655
luschool <35786777+luschool@users.noreply.github.com>
2018-04-04 19:06:59 -0500

    Week 12 Scripts

---
+++ chapter11extra.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Week 12 script by Lucas Merriott
+# Made in ArchLinux so commands may differ
+# Extra commands that will be includedand executed from main script
+
+
+echo "Importing commands from chapter11extra.sh and running them in this terminal shell instance."
+echo
+ls -l
+echo
+for str in one two three four; do
+	echo "$str"
+done
